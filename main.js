cbb475ff7f9d2e455579ac5e10b05454993ddca2
Mike <michael.priestley@postgrad.manchester.ac.uk>
2017-01-10 23:28:58 +0000

    improved questions.json. started on event listener. started injecting html with js

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 var SCORE = 0;
+var QUESTION_OBJECT = {};
 
 function loadJSON(filename, callback) {
   var xobj = new XMLHttpRequest();
@@ -8,20 +9,50 @@ function loadJSON(filename, callback) {
     if (xobj.readyState == 4 && xobj.status == "200") {
       // .open will NOT return a value but simply returns
       // undefined in async mode so use a callback
-      callback(xobj.responseText);
+      var jsonresponse = callback(xobj.responseText);
+      return jsonresponse
     }
   }
   xobj.send(null);
 }
 
-// Load question information
-loadJSON('questions.json', function(response) {
-jsonresponse = JSON.parse(response);
-console.log(jsonresponse[0]);
-});
 
-// load answer information
-loadJSON('answers.json', function(response) {
-jsonresponse = JSON.parse(response);
-console.log(jsonresponse[0]);
+// Load question information
+question_object = loadJSON('questions.json', function(response) {
+  var question_object = JSON.parse(response)[0];
+
+    // populate questions
+    document.getElementById("question").innerHTML = question_object.Q1.Question;
+    var answer_list = document.getElementById("ulist");
+
+
+    // populate answers
+    Object.keys(question_object.Q1.Answers).forEach(function(option){
+
+        var info = question_object.Q1.Answers[option];
+        var li = document.createElement('li');
+        li.setAttribute("id", option);
+        li.innerHTML = '<a>'+info.Answer+'</a>';
+
+        // Add click event listener to each li element
+        // change color
+        // add to total
+        // grey out buttons
+        li.addEventListener("click", function (a) {
+          SCORE += info.Points;
+          console.log(option, SCORE);
+          if (info.Points == 1){
+            li.style.backgroundColor = "green";
+          } else {
+            li.style.backgroundColor = "red";
+            document.getElementById('A').style.backgroundColor = "green";
+          }
+        });
+
+        answer_list.appendChild(li);
+
+    });
+
+
+
 });

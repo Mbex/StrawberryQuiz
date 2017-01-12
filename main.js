1e4ea096c4ae7f98344a26e0a5d4bd46bda4384b
Mike <michael.priestley@postgrad.manchester.ac.uk>
2017-01-12 09:09:59 +0000

    better functionalised js

--- main.js
+++ main.js
@@ -1,58 +1,90 @@
+//GLOBALS
 var SCORE = 0;
 var QUESTION_OBJECT = {};
 
-function loadJSON(filename, callback) {
-  var xobj = new XMLHttpRequest();
-  xobj.overrideMimeType("application/json");
-  xobj.open('GET', filename, true);
-  xobj.onreadystatechange = function () {
-    if (xobj.readyState == 4 && xobj.status == "200") {
-      // .open will NOT return a value but simply returns
-      // undefined in async mode so use a callback
-      var jsonresponse = callback(xobj.responseText);
-      return jsonresponse
-    }
-  }
-  xobj.send(null);
+// FUNCTIONS
+function loadJSON(filename) {
+
+    return new Promise( function(resolve, reject) {
+
+      var xobj = new XMLHttpRequest();
+      xobj.overrideMimeType("application/json");
+      xobj.open('GET', filename, true);
+      xobj.addEventListener('load', function(evt){
+          resolve(JSON.parse(xobj.responseText)[0]);
+      });
+      xobj.addEventListener('error', function(error){
+          reject(error);
+      })
+      xobj.send(null);
+    });
 }
 
+function getCorrectAnswer(question_object, qIndex) {
 
-// Load question information
-question_object = loadJSON('questions.json', function(response) {
-  var question_object = JSON.parse(response)[0];
+  return new Promise( function (resolve, reject){
 
-    // populate questions
-    document.getElementById("question").innerHTML = question_object.Q1.Question;
-    var answer_list = document.getElementById("ulist");
-
-
-    // populate answers
-    Object.keys(question_object.Q1.Answers).forEach(function(option){
-
-        var info = question_object.Q1.Answers[option];
-        var li = document.createElement('li');
-        li.setAttribute("id", option);
-        li.innerHTML = '<a>'+info.Answer+'</a>';
-
-        // Add click event listener to each li element
-        // change color
-        // add to total
-        // grey out buttons
-        li.addEventListener("click", function (a) {
-          SCORE += info.Points;
-          console.log(option, SCORE);
-          if (info.Points == 1){
-            li.style.backgroundColor = "green";
-          } else {
-            li.style.backgroundColor = "red";
-            document.getElementById('A').style.backgroundColor = "green";
-          }
-        });
-
-        answer_list.appendChild(li);
-
+    var Ans = question_object[qIndex].Answers;
+    Object.keys(Ans).forEach(function(option){
+      if (Ans[option].Points == 1){
+        resolve(option);
+      };
+      reject(error);
     });
+  });
+}
+
+function populateQAdivs(question_object, qIndex, correctAns) {
+
+  // Populate Question div
+  document.getElementById("question").innerHTML = question_object[qIndex].Question;
+
+  // Populate Answer div
+  var answer_list = document.getElementById("ulist");
+  Object.keys(question_object[qIndex].Answers).forEach(function(option){
+
+    var info = question_object[qIndex].Answers[option];
+    var li = document.createElement('li');
+    li.setAttribute("id", option);
+    li.innerHTML = '<a>'+info.Answer+'</a>';
+
+    li = evtButtonClick(li, correctAns);
+
+    answer_list.appendChild(li);
+
+  });
+}
+
+function evtButtonClick(li, correctAns) {
+
+  li.addEventListener("click", function () {
+
+    // Change colour of div
+    if (li.id == correctAns){
+      li.style.backgroundColor = "green";
+      SCORE += 1;
+    } else {
+      li.style.backgroundColor = "red";
+    };
+  });
+  return li;
+};
 
 
+// MAIN
+loadJSON("questions.json").then(function(QUESTION_OBJECT){
 
+  var q = "Q1";
+
+  getCorrectAnswer(QUESTION_OBJECT, q).then(function(correctAns){
+
+    populateQAdivs(QUESTION_OBJECT, q, correctAns);
+
+
+  });
 });
+
+
+// Add event listener to:
+//    grey out buttons
+//    repopulate div

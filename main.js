660309f054cd4c20ea18d05a62e8668ed700467f
Mike <michael.priestley@postgrad.manchester.ac.uk>
2017-01-12 22:37:40 +0000

    click event colours and blank div finished.

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 //GLOBALS
 var SCORE = 0;
-var QUESTION_OBJECT = {};
+QUESTION_OBJECT = {};
+var CLICKS = 0;
 
 // FUNCTIONS
 function loadJSON(filename) {
@@ -26,15 +27,15 @@ function getCorrectAnswer(question_object, qIndex) {
 
     var Ans = question_object[qIndex].Answers;
     Object.keys(Ans).forEach(function(option){
+
       if (Ans[option].Points == 1){
         resolve(option);
       };
-      reject(error);
     });
   });
 }
 
-function populateQAdivs(question_object, qIndex, correctAns) {
+function populateQAdivs(question_object, qIndex, correctAns, allAns) {
 
   // Populate Question div
   document.getElementById("question").innerHTML = question_object[qIndex].Question;
@@ -46,45 +47,82 @@ function populateQAdivs(question_object, qIndex, correctAns) {
     var info = question_object[qIndex].Answers[option];
     var li = document.createElement('li');
     li.setAttribute("id", option);
-    li.innerHTML = '<a>'+info.Answer+'</a>';
+    li.innerHTML = '<span>'+info.Answer+'</span>';
 
-    li = evtButtonClick(li, correctAns);
+    li = evtButtonClick(li, correctAns, allAns, question_object, qIndex);
 
     answer_list.appendChild(li);
 
   });
 }
 
-function evtButtonClick(li, correctAns) {
+function evtButtonClick(li, correctAns, allAns, question_object, qIndex) {
 
   li.addEventListener("click", function () {
 
-    // Change colour of div
-    if (li.id == correctAns){
-      li.style.backgroundColor = "green";
+    // Grey out other options
+    allAns.forEach( function(ea) {
+      document.getElementById(ea).style.backgroundColor = "grey";
+    });
+
+    // Change colour of div based on correct answer
+    if (this.id == correctAns){
+      this.style.backgroundColor = "green";
       SCORE += 1;
     } else {
-      li.style.backgroundColor = "red";
+      this.style.backgroundColor = "red";
+      document.getElementById(correctAns).style.backgroundColor = "green";
     };
+
+    // Make rest of screen unresponsive
+    var overlay = document.createElement("div");
+    overlay.className = "overlay";
+    var wrapper = document.getElementById('wrapper');
+    wrapper.appendChild(overlay);
+
+    // Get rid of contents
+    window.setTimeout(function () {
+      document.getElementById("question").innerHTML = "";
+      document.getElementById("ulist").innerHTML = "";
+      loadQuestion(question_object, qIndex);  !!!!!!!!!!!!!!!!!!!!
+    }, 3000);
+
+    // move question on
+    q = qs + 1 !!!!!!!!!!!!!!!!!!!!
+
+    // Remove overlay (blank div)
+    overlay.remove()
+
+   console.log("SCORE: ", SCORE);
   });
+
   return li;
+
+};
+
+function loadQuestion(QUESTION_OBJECT, q) {
+  getCorrectAnswer(QUESTION_OBJECT, q).then(function(correctAns){
+    var allAns = Object.keys(QUESTION_OBJECT[q].Answers);
+    populateQAdivs(QUESTION_OBJECT, q, correctAns, allAns);
+  });
 };
 
 
 // MAIN
 loadJSON("questions.json").then(function(QUESTION_OBJECT){
 
-  var q = "Q1";
+  var i = 0;
+  var qs = Object.keys(QUESTION_OBJECT);
+  var q = qs[i];
 
-  getCorrectAnswer(QUESTION_OBJECT, q).then(function(correctAns){
+  loadQuestion(QUESTION_OBJECT, q);
 
-    populateQAdivs(QUESTION_OBJECT, q, correctAns);
-
-
-  });
 });
 
 
 // Add event listener to:
 //    grey out buttons
 //    repopulate div
+
+
+// restart has to reset score
